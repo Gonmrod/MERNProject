767c3c943ad7738b8640b397347b87d4b47b2c98
Gonzalo Rodriguez <gonzalo.marodriguez@gmail.com>
2023-12-18 19:46:18 -0300

    Rollup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      external: ['/frontend/src/main.jsx'],
+  },
+},
+  plugins: [react()]
 })
 
