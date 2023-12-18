cef12e838ae7054d61eecbc1f4c38bf58c06f4e0
Gonzalo Rodriguez <gonzalo.marodriguez@gmail.com>
2023-12-18 19:21:51 -0300

    asd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
+ /*  build: {
     manifest: true,
     rollupOptions: {
       input: '/frontend/src/main.jsx'
   },
-},
+}, */
   plugins: [react()]
 })
 
