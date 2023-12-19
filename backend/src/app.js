69803ab12d83e6cd173f017cb873434f798e3fa0
Gonzalo Rodriguez <gonzalo.marodriguez@gmail.com>
2023-12-19 09:47:07 -0300

    dist en carpeta public

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,13 +27,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors())
 app.use(express.json());
-/* app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    res.header('Access-Control-Allow-Credentials', 'true')
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
     next()
-}) */
+}) 
 app.use(winston);
 app.use(express.static((`${__dirname}/public`)))
 app.use(express.urlencoded({extended: true}));
