2980ea795444a09f10c057045030f0be522a3ae8
Gonzalo Rodriguez <gonzalo.marodriguez@gmail.com>
2023-12-19 09:09:29 -0300

    Change Cors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,9 +28,9 @@ app.use(passport.session());
 app.use(cors())
 app.use(express.json());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://deploytest-n6dd.onrender.com')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Credentials', 'true')
     next()
 })
